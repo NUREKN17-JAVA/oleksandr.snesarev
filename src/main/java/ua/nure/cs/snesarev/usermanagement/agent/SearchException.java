4e1cc46ce0076eb2f113e05aaa5eb64ec831c0aa
snesarevv <oleksandr.snesariev@nure.ua>
2019-12-28 02:55:21 +0200

    lab5

---
+++ src/main/java/ua/nure/cs/snesarev/usermanagement/agent/SearchException.java
@@ -0,0 +1,11 @@
+package ua.nure.kn.snesarev.usermanagement.agent;
+
+public class SearchException extends Exception {
+    public SearchException(Throwable cause) {
+        super(cause);
+    }
+
+    public SearchException(String message) {
+        super(message);
+    }
+}
